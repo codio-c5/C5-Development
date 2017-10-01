b6d9de71a288d4084fa3950f8e22ee066f49e989
Paul Sery <pgsery@gmail.com>
2017-10-01 18:28:10 +0000

    add error checking to cleanup.sh

--- rsa/cleanup.sh
+++ rsa/cleanup.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 #
+cd ~/workspace
+#
 if [ -d lab-part1 ]; then
 	rm -vf lab-part1/*
+else
+	mkdir lab-part1
 fi
 if [ -d lab-part2 ]; then
 	rm -vf lab-part2/*
 	echo "" > lab-part2/message.txt
+	echo "" > lab-part2/message.txt
+else
+	mkdir lab-part2
 fi
 if [ -d lab-part3 ]; then
 	rm -vf lab-part3/*
+	echo "" > lab-part3/partner-message.txt
+else
+	mkdir lab-part3
 	echo "" > lab-part3/partner-message.txt
 fi
